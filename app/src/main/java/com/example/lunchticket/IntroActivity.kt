60f92b47a4a0a51306e424cb788e094966806fd6
stban2703 <s.t.ban1401@gmail.com>
2021-11-25 23:59:48 -0500

    Added colors and logo screen

---
+++ app/src/main/java/com/example/lunchticket/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.lunchticket
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(3000)
+            val intent = Intent(this@IntroActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
