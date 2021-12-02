258255687df791c042239d876ded60b69457641b
Danna García <1193251859@u.icesi.edu.co>
2021-12-02 14:51:02 -0500

    Post and firebase ready

--- app/src/main/java/com/example/lunchticket/CreatePostActivity.kt
+++ app/src/main/java/com/example/lunchticket/CreatePostActivity.kt
@@ -1,8 +1,14 @@
 package com.example.lunchticket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.lunchticket.databinding.ActivityCreatePostBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class CreatePostActivity : AppCompatActivity() {
 
@@ -14,8 +20,22 @@ class CreatePostActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.createBackBtn.setOnClickListener {
-            finish()
+        binding.sendPostBtn.setOnClickListener {
+            val postId = UUID.randomUUID().toString();
+            val postTitle = binding.postTitleET.text.toString()
+            val postDescription = binding.postDescriptionET.text.toString()
+            val postDate = Calendar.getInstance().time.time
+            val post = Post(postId, postTitle, postDescription, postDate)
+            Firebase.firestore.collection("posts").document(postId).set(post)
+            Toast.makeText(this, "Publicación creada con éxito", Toast.LENGTH_SHORT).show()
+
         }
+
+        binding.createBackBtn.setOnClickListener {
+            val intent = Intent(this, PostListActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
